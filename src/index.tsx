ff68b49d135435cdb90745ba56f3628a7363dd9c
Ramos Posadas <959055@cognizant.com>
2023-04-05 14:06:21 -0500

    added initial package bundling methods

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,39 @@
 import * as esbuild from "esbuild-wasm";
 import ReactDOM from "react-dom/client";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import React from "react";
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 
 const App = () => {
+  const ref = useRef<any>();
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
 
   const startService = async () => {
-    const service = await esbuild.startService({
+    ref.current = await esbuild.startService({
       worker: true,
       wasmURL: "/esbuild.wasm",
     });
-    console.log(service);
   };
   useEffect(() => {
     startService();
   }, []);
-  const onClick = () => {
-    console.log("clicked");
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+    const result = await ref.current.build({
+      entryPoints: ["index.js"],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+      define: {
+        "process.env.NODE_ENV": '"production"',
+        global: "window",
+      },
+    });
+
+    setCode(result.outputFiles[0].text);
   };
 
   return (
