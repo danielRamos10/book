2bc6f2b4df2139887a9c5c352643e70ad66c4391
Ramos Posadas <959055@cognizant.com>
2023-04-11 18:32:28 -0500

    adde some styling to format buttton

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -2,6 +2,7 @@ import MonacoEditor, { EditorDidMount } from "@monaco-editor/react";
 import prettier from "prettier";
 import parser from "prettier/parser-babel";
 import { useRef } from "react";
+import './code-editor.css';
 interface CodeEditorProps {
   initialValue: string;
   onChange(value: string): void;
@@ -23,12 +24,17 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
       useTabs: false,
       semi: true,
       singleQuote: true,
-    });
+    }).replace(/\n$/, '');
     editorRef.current.setValue(formattedVal);
   };
   return (
-    <div>
-      <button onClick={onFormatClick}>Format</button>
+    <div className="editor-wrapper">
+      <button
+        className="button button-format is-primary is-small"
+        onClick={onFormatClick}
+      >
+        Format
+      </button>
       <MonacoEditor
         editorDidMount={onEditorDidMount}
         value={initialValue}
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 import { fetchPlugin } from "./plugins/fetch-plugin";
 import CodeEditor from "./components/code-editor";
+import 'bulmaswatch/superhero/bulmaswatch.min.css'
 
 const App = () => {
   const ref = useRef<any>();
